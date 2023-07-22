0dfbd4cad9488e5605520a1551a25d211650d4ca
Royshivani <7780097@gmail.com>
2023-07-22 19:56:09 +0530

    App import name updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App.js";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "./global.css";
